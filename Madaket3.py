9773198ebd75b8167785e992f2bc586d087e1088
ryanflynn9299 <35703007+ryanflynn9299@users.noreply.github.com>
2018-02-26 14:16:17 -0500

    Create Madaket3.py
    Python 3.6 version

---
+++ Madaket3.py
@@ -0,0 +1,26 @@
+'''
+Ryan Flynn
+January 2018
+Mr. Maki
+Advanced Computer Science
+Personal Assistant: Madaket
+v2.0
+'''
+
+import wolframalpha, wikipedia
+
+
+while True:
+    inp = input('How can I help you?/n')
+    try:
+        app_id = 'L96JHW-WUHKT49E78'
+
+        client = wolframalpha.Client(app_id)
+
+        result = client.query(inp)
+        answer = next(result.results).text
+
+        print(answer)
+    except Exception as e:
+        print(e)
+        print(wikipedia.summary(input, sentences = 2))
